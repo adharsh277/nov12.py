09ef9eb04e0bd385ccc966526b795f53732f33ae
adharsh.U <114822057+adharsh277@users.noreply.github.com>
2022-11-12 13:09:03 +0530

    Add files via upload

---
+++ pythontest12344.py
@@ -0,0 +1,43 @@
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+thisdict1 = {
+    "brand": "maruthi",
+    "model": "suzuki",
+    "year": 1962
+}
+mydict1 = thisdict1.copy()
+mydict = thisdict.copy()
+print(mydict)
+print(mydict1)
+# 548
+myfamily = {
+    "child1": {
+        "name": "Emil",
+        "year": 2004
+    },
+    "child2": {
+        "name": "Tobias",
+        "year": 2007
+    },
+    "child3": {
+        "name": "Linus",
+        "year": 2011
+    }
+}
+
+print(myfamily)
+# 566
+a = 50
+b = 10
+print(a == b)
+
+print(a-b)
+print(a+b)
+print(a < b)
+print(a/b)
+print(a > b)
+print(a//b)
+print(a-b)
